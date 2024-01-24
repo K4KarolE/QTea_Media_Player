7c4c2bcf6ccfa1367525c6a886dadce0720958d3
k4karole <k4karole@gmail.com>
2024-01-24 06:12:46 +0000

    learned: scrollbar to qwidget window / tab widget

---
+++ docs/learning/set_scrollbar_to_qwidget_window.py
@@ -0,0 +1,69 @@
+
+from PyQt6.QtWidgets import (
+    QApplication,
+    QWidget,
+    QLabel,
+    QScrollArea,
+    QScrollBar
+    )
+
+import sys
+
+''' APP '''
+app = QApplication(sys.argv)
+
+
+'''
+QSCROLLAREA WINDOW <-- QWIDGET WINDOW <-- QWIDGETS
+'''
+WINDOW_WIDTH, WINDOW_HEIGHT = 220, 400
+window_main = QScrollArea()
+window_widgets = QWidget()
+
+''' SCROLL BARS '''
+scroll_bar_name_ver = QScrollBar()
+scroll_bar_name_ver.setStyleSheet(
+                                "QScrollBar::vertical"
+                                    "{"
+                                    "width: 10px;"
+                                    "}"
+                                )
+
+scroll_bar_name_hor = QScrollBar()
+scroll_bar_name_hor.setStyleSheet(
+                                "QScrollBar::horizontal"
+                                    "{"
+                                    "height: 0px;"
+                                    "}"
+                                )
+
+window_main.setVerticalScrollBar(scroll_bar_name_ver)
+window_main.setHorizontalScrollBar(scroll_bar_name_hor)
+
+''' WIDGETS '''
+counter = 0
+counter_max = 30
+while counter != counter_max:
+    text = QLabel(f'{counter} - RANDOM TEXT', window_widgets)
+    text.move(50, 20 + counter*30)
+    counter += 1
+
+'''
+LEARNED
+The scrollbars visibility depends on the window_widgets window size,
+not the widgets the window_widgets window contains
+- window_widgets.resize(WINDOW_WIDTH-20, (counter_max + 1)*30)
+  - WINDOW_WIDTH - 20 --> the horizontal scrollbar invisible
+  - or as above set a 0px horizontal scrollbar --> scrollbar invisible
+  - (counter_max + 1)*30 --> makes the vertical scrollbar dependent
+    on the widgets amount
+'''
+window_main.resize(WINDOW_WIDTH, WINDOW_HEIGHT)
+window_widgets.resize(WINDOW_WIDTH, (counter_max + 1)*30)
+
+
+window_main.setWidget(window_widgets)
+
+window_main.show()
+
+sys.exit(app.exec())
---
+++ docs/learning/set_scrollbar_to_tab_widget.py
@@ -0,0 +1,73 @@
+
+from PyQt6.QtWidgets import (
+    QApplication,
+    QWidget,
+    QLabel,
+    QScrollArea,
+    QScrollBar,
+    QTabWidget
+    )
+
+import sys
+
+''' APP '''
+app = QApplication(sys.argv)
+
+
+'''
+MAIN WINDOW <-- TAB WIDGET <-- QSCROLLAREA WINDOW <-- QWIDGET WINDOW <-- QWIDGETS
+'''
+WINDOW_WIDTH, WINDOW_HEIGHT = 220, 400
+window_main = QWidget()
+window_scrollarea = QScrollArea()
+window_widgets = QWidget()
+tab_widget = QTabWidget()
+
+
+''' SCROLL BARS '''
+scroll_bar_name_ver = QScrollBar()
+scroll_bar_name_ver.setStyleSheet(
+                                "QScrollBar::vertical"
+                                    "{"
+                                    "width: 10px;"
+                                    "}"
+                                )
+
+scroll_bar_name_hor = QScrollBar()
+scroll_bar_name_hor.setStyleSheet(
+                                "QScrollBar::horizontal"
+                                    "{"
+                                    "height: 0px;"  # -> not visible
+                                    "}"
+                                )
+
+window_scrollarea.setVerticalScrollBar(scroll_bar_name_ver)
+window_scrollarea.setHorizontalScrollBar(scroll_bar_name_hor)
+
+
+''' QWIDGET WINDOW <-- QWIDGETS '''
+counter = 0
+counter_max = 30
+while counter != counter_max:
+    text = QLabel(f'{counter} - RANDOM TEXT', window_widgets)
+    text.move(50, 20 + counter*30)
+    counter += 1
+window_widgets.resize(WINDOW_WIDTH, (counter_max + 1)*30)
+
+
+''' QSCROLLAREA WINDOW <-- QWIDGET WINDOW '''
+window_scrollarea.setWidget(window_widgets)
+
+
+''' TAB WIDGET <-- QSCROLLAREA WINDOW '''
+tab_widget.addTab(window_scrollarea, 'Test - 1')
+tab_widget.addTab(QWidget(), 'Test - 2 - Empty tab') # JUST FOR TO BE A 2ND TAB
+tab_widget.setGeometry(20, 20, WINDOW_WIDTH, WINDOW_HEIGHT)
+
+
+''' MAIN WINDOW <-- TAB WIDGET '''
+tab_widget.setParent(window_main)
+
+window_main.show()
+
+sys.exit(app.exec())
