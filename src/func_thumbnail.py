3dd6ea7a79b5264be66e86b2b2a8b68a6681bb4e
k4karole <k4karole@gmail.com>
2025-04-26 05:22:24 +0100

    thumbnail funcs comments added phase 2

--- src/func_thumbnail.py
+++ src/func_thumbnail.py
@@ -30,7 +30,9 @@ current_time = int(time())
 """
 # THUMBNAIL PL
 def start_thumbnail_thread_grouped_action():
-    """ Actioned vie the Thumbnail View button """
+    """ Actioned via the Thumbnail View button
+        Standard playlist >> Thumbnail playlist
+    """
     if is_new_thumbnail_generation_needed():
         stop_another_playlist_thumbnail_thread()
         update_thumbnail_support_vars_before_thumbnail_thread()
@@ -70,8 +72,8 @@ def stop_another_playlist_thumbnail_thread():
     """ To make sure there are no multiple thumbnail generation
         is running for different playlists
         If one is running while another thumbnail generation
-        is triggered via the Thumbnail View button, the first
-        will be stopped and the new one will start
+        is triggered via the Thumbnail View button, the currently
+        running will be stopped and the new one will start
     """
     if cv.thumbnail_db_table:
         widgets_window = cv.playlist_widget_dic[cv.thumbnail_db_table]['thumbnail_window'].widgets_window
@@ -83,14 +85,14 @@ def stop_another_playlist_thumbnail_thread():
 # THUMBNAIL PL / start_thumbnail_thread_grouped_action()
 def update_thumbnail_support_vars_before_thumbnail_thread():
     """ Once the thumbnail generation triggered,
-        snapshot the crucial vars to make sure
+        it snapshots the crucial vars to make sure
         switching playlist is not causing any issue
-        while the thumb. gen. is still in progress
+        while the thumbnail generation is still in progress
     """
     cv.thumbnail_db_table = cv.active_db_table
     cv.thumbnail_pl_tracks_count = cv.active_pl_name.count()
 
-    cv.thumbnail_last_played_track_index = settings[cv.active_db_table]['last_track_index']
+    cv.thumbnail_last_played_track_index = cv.active_pl_last_track_index
     cv.thumbnail_last_played_track_index_is_valid = -1 < cv.thumbnail_last_played_track_index <=cv.thumbnail_pl_tracks_count-1
 
     cv.thumbnail_last_selected_track_index = cv.active_pl_name.currentRow()
@@ -120,12 +122,11 @@ def create_new_thumbnail_widgets_window():
 
 # THUMBNAIL PL / start_thumbnail_thread_grouped_action()
 def generate_thumbnail_dic():
-    """ Generating thumbnails widgets, supporting and validation dictionary
-
+    """ Generating thumbnails widgets, support and validation dictionary
         Why save the current image size for every playlist (thumbnail_img_size)?
-        - To make sure if an existing thumbnail playlist needs to be
-        regenerated after the thumbnail image size update
-        via Settings window / General tab
+            To make sure if an existing thumbnail playlist needs to be
+            regenerated after the thumbnail image size update
+            via Settings window / General tab
     """
     path_list, duration_list = get_all_playlist_path_from_db()
     if path_list:
@@ -155,10 +156,12 @@ def generate_thumbnail_dic():
 # THUMBNAIL PL / generate_thumbnail_dic()
 def get_all_playlist_path_from_db():
     """ Get all the media of the playlist switched to thumbnail view
+        Scenario - outside this function:
         If the thumbnail view button clicked when adding media to
         the standard playlist is still in progress, the thumbnail view
-        track list will be less than the standard playlist:
-        Information message will be displayed
+        track list will be less than the standard playlist and a
+        warning message will be displayed via:
+        msg_box_for_thumbnail_view_when_adding_tracks_to_pl_unfinished()
     """
     result_all = sql_cursor.execute("SELECT * FROM {0}".format(cv.thumbnail_db_table)).fetchall()
     duration_list = [duration[1] for duration in result_all]
@@ -168,6 +171,14 @@ def get_all_playlist_path_from_db():
 
 # ACTIVE & THUMBNAIL PL / start_thumbnail_thread_grouped_action()
 def thumbnail_widget_resize_and_move_to_pos():
+    """
+    This function resizes and moves the thumbnail widgets into position
+    Scenario - after new thumbnail playlist generation:
+        The thumbnails` generated in the generate_thumbnail_dic(),
+        position is not defined yet, just added to their parent window
+    Scenario - thumbnail playlist already exists + thumbnail playlist size change:
+        New thumbnail widget size and position calculation needed
+    """
     thumbnail_widget_dic = cv.playlist_widget_dic[cv.active_db_table]['thumbnail_widgets_dic']
     if thumbnail_widget_dic:
         thumbnail_counter = 0
@@ -184,11 +195,14 @@ def thumbnail_widget_resize_and_move_to_pos():
             thumbnail_widget_dic[thumbnail_index]["widget"].move(thumbnail_pos_x, thumbnail_pos_y)
             thumbnail_widget_dic[thumbnail_index]["widget"].resize(cv.thumbnail_new_width, cv.thumbnail_height)
             thumbnail_counter += 1
-
         update_window_widgets_size(thumbnail_pos_y)
+
 
 # ACTIVE & THUMBNAIL PL / thumbnail_widget_resize_and_move_to_pos()
 def generate_thumbnail_widget_new_width():
+    """ Thumbnail widget width depends on:
+        Thumbnail playlist`s width / thumbnail img size / thumbnail amount
+     """
     available_space = cv.thumbnail_main_window_width - cv.scroll_bar_size - 2 * cv.thumbnail_pos_base_x + cv.thumbnail_pos_gap
     thumbnail_and_gap = cv.thumbnail_width + cv.thumbnail_pos_gap
     thumbnails_in_row_possible = int(available_space / thumbnail_and_gap)
@@ -205,6 +219,12 @@ def generate_thumbnail_widget_new_width():
 
 # ACTIVE & THUMBNAIL PL / thumbnail_widget_resize_and_move_to_pos()
 def update_window_widgets_size(last_thumbnail_pos_y):
+    """ To update the thumbnail widgets parent window`s size
+        >> to make sure to use all the available space
+        >> to make sure the scrollbar is up to date**
+        **it is widget window dependent:
+        ThumbnailMainWindow(QScrollArea) << WidgetsWindow(QWidget)
+    """
     window_widgets_height = last_thumbnail_pos_y + cv.thumbnail_height + cv.thumbnail_pos_base_y
     thumbnail_widgets_window = cv.playlist_widget_dic[cv.active_db_table]['thumbnail_window'].widgets_window
     thumbnail_widgets_window.resize(cv.thumbnail_main_window_width, window_widgets_height)
@@ -270,6 +290,7 @@ def create_thumbnails_and_update_widgets(index):
                 result = "failed"
     return str(result)
 
+
 # NONE / create_thumbnails_and_update_widgets()
 def get_time_frame_taken_from(vid_duration):
     if vid_duration:
@@ -289,8 +310,7 @@ def get_time_frame_taken_from(vid_duration):
 def update_thumbnail_support_vars_before_playlist_clear():
     """ When the playlist clear actioned the row change signal triggered
         earlier before the "cv.active_pl_name.count()" get the 0 value >>
-        values have to be set manually >>
-        avoid style update on deleted thumbnails
+        values have to be set manually to avoid style update on deleted thumbnails
     """
     cv.active_pl_tracks_count = 0
     cv.current_track_index = -1
@@ -300,12 +320,20 @@ def update_thumbnail_support_vars_before_playlist_clear():
 
 # ACTIVE PL
 def switch_to_standard_active_playlist_from_thumbnail_pl():
+    """ Scenarios:
+        - Add media(s) or folder to the playlist
+        - Remove one or all media from the playlist
+    """
     if not cv.active_pl_name.isVisible():
         br.button_thumbnail.button_thumbnail_clicked()
 
 
 # THUMBNAIL PL
 def stop_thumbnail_thread():
+    """ Scenarios:
+        - Thumbnail generation is in progress + switching back to the standard playlist
+        - Before removing thumbnails
+    """
     if cv.thumbnail_db_table:
         widgets_window = cv.playlist_widget_dic[cv.thumbnail_db_table]['thumbnail_window'].widgets_window
         if widgets_window.thread_thumbnails_update.isRunning():
@@ -341,7 +369,7 @@ def update_thumbnail_view_button_style_after_playlist_change():
 
 # PLAYING PL
 def update_playing_and_previous_thumbnail_style():
-    """ Updating thumbnail style after new track is started
+    """ Updating thumbnail style after new media is started
         in func_play_coll.py / play_track()
     """
     thumbnail_widget_dic = cv.playlist_widget_dic[cv.playing_db_table]['thumbnail_widgets_dic']
@@ -389,13 +417,15 @@ def msg_box_for_thumbnail_view_when_adding_tracks_to_pl_unfinished():
             MyMessageBoxConfirmation(
                 'Adding media is unfinished',
                 '\nPlease note, QTea is still adding media to the playlist.\n\n'
-                'The thumbnail view not mirrors the standard playlist completely.\n\n'
-                'Come back to the thumbnail view later for the full list.'
+                'The thumbnail view will not mirror the standard playlist completely.\n\n'
+                'Come back to the thumbnail view later for the full list once\n'
+                'the addition is completed.'
                 )
 
 
 # NONE
 def msg_box_wrapper_for_remove_all_thumbnails_and_clear_history():
+    """ Settings window / Purge Thumbnails button """
     MyMessageBoxConfReq(
         "Are you sure you want to delete all the thumbnails\n"
         "and clear the thumbnail history?",
@@ -408,10 +438,12 @@ def remove_all_thumbnails_and_clear_history():
     stop_thumbnail_thread()
     switch_all_pl_to_standard_from_thumbnails_view(True)
     try:
+        # THUMBNAILS
         files = glob.glob(f'{PATH_THUMBNAILS}/*')
         for file in files:
             if Path(file).suffix != '.json':
                 os.remove(file)
+        # HISTORY
         thumbnail_history["failed"] = {}
         thumbnail_history["completed"] = {}
         save_thumbnail_history_json()
@@ -428,8 +460,8 @@ def remove_all_thumbnails_and_clear_history():
 # NONE
 def switch_all_pl_to_standard_from_thumbnails_view(triggered_via_purge_thumbnails_button = False):
     """ Triggered by Settings window / Save button
-        To make sure all playlist which is going to be hidden
-        switched back to the standard playlist view
+        To make sure all playlists which are going to be hidden
+        are switched back to the standard playlist view
     """
     def switch_to_standard_pl(pl_name):
         if not cv.playlist_widget_dic[pl_name]['name_list_widget'].isVisible():
@@ -451,7 +483,9 @@ def switch_all_pl_to_standard_from_thumbnails_view(triggered_via_purge_thumbnail
 
 #NONE
 def auto_thumbnails_removal_after_app_closure():
-    """ Auto thumbnail removal field validation in
+    """ Removes the thumbnails and updates the thumbnail_history json
+        automatically when quitting QTea
+        Auto thumbnail removal after "x" days field validation in
         window_settings.py / general_fields_validation()
         cv.thumbnail_remove_older_than:
         0 - every day
