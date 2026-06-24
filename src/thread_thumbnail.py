c1c008133cca13b4247b672af5009c417792abbf
k4karole <k4karole@gmail.com>
2026-06-24 05:56:51 +0100

    run() refactored - cv.active_db_table >> cv.thumbnail_db_table

--- src/thread_thumbnail.py
+++ src/thread_thumbnail.py
@@ -12,16 +12,18 @@ class ThreadThumbnail(QThread):
         self.is_thumbnail_thread_stopped = False
 
     def run(self):
-        thumbnail_widgets_dic = cv.playlist_widget_dic[cv.active_db_table]['thumbnail_widgets_dic']
-        thumbnail_window_validation = cv.playlist_widget_dic[cv.active_db_table]['thumbnail_window_validation']
+        thumbnail_widgets_dic = cv.playlist_widget_dic[cv.thumbnail_db_table]['thumbnail_widgets_dic']
+        thumbnail_window_validation = cv.playlist_widget_dic[cv.thumbnail_db_table]['thumbnail_window_validation']
         thumbnail_window_validation['thumbnail_generation_needed'] = True   # un-completed thumbnail thread
         if thumbnail_widgets_dic:
             for index in thumbnail_widgets_dic:
-                if self.is_thumbnail_thread_stopped:
+                if not self.is_thumbnail_thread_stopped:
+                    # result = "audio" / "failed" / thumbnail img path
+                    result = create_thumbnails_and_update_widgets(index)
+                    if result:
+                        self.result_ready.emit(index, result)
+                else:
                     return
-                # result = "audio" / "failed" / thumbnail img path
-                result = create_thumbnails_and_update_widgets(index)
-                self.result_ready.emit(index, result)
             save_thumbnail_history_json()
             thumbnail_window_validation['thumbnail_generation_needed'] = False  # completed thumbnail thread
 
