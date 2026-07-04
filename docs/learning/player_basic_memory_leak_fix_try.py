ae69d5582ce2b7ba4da56844d02d64d6bb9809b9
k4karole <k4karole@gmail.com>
2026-07-04 12:48:16 +0100

    "QMediaPlayer().setSource()" memory leak test

--- docs/learning/player_basic_memory_leak_fix_try.py
+++ docs/learning/player_basic_memory_leak_fix_try.py
@@ -1,15 +1,15 @@
 """
-The media file paths need to be added (self.path_1, self.path_2)
-
-App freezing when trying to start a new media ("Play 1st" button >> "Play 2nd" button)
-More info about the workaround in the "src / av_player"
-
 Switching there and back between the two media >> can increase the memory usage
 It is the memory leak of the "QMediaPlayer().setSource()" function
+
+Was trying to create a new AVPlayer class instance for every media play and remove the previous instance
+>> The memory leak issue is still in place
+
+For testing, do not forget to add media paths to the "path_1" , "path_2" variables
 """
 
-from PyQt6.QtMultimedia import QAudioOutput, QMediaPlayer
 from PyQt6.QtCore import QUrl
+from PyQt6.QtMultimedia import QAudioOutput, QMediaPlayer
 from PyQt6.QtMultimediaWidgets import QVideoWidget
 from PyQt6.QtWidgets import (
     QApplication,
@@ -20,6 +20,9 @@ from PyQt6.QtWidgets import (
 
 import sys
 from pathlib import Path
+
+path_1 = str(Path(""))
+path_2 = str(Path(""))
 
 
 class AVPlayer(QWidget):
@@ -36,11 +39,7 @@ class AVPlayer(QWidget):
         self.player.setAudioOutput(self.audio_output)
         self.player.mediaStatusChanged.connect(self.media_status_changed_action)
 
-        self.path_1 = str(Path("FILE PATH NEEDED"))
-        self.path_2 = str(Path("FILE PATH NEEDED"))
-
     def play_media(self, media_path):
-        self.player.stop()
         self.player.setSource(QUrl.fromLocalFile(media_path))
 
     def media_status_changed_action(self):
@@ -55,18 +54,32 @@ window = QWidget()
 window.resize(500,500)
 window.setWindowTitle("Basic Player")
 
-av_player = AVPlayer()
+av_player_list = [AVPlayer()]
 
 button_1 = QPushButton("Play 1st")
-button_1.clicked.connect(lambda: av_player.play_media(av_player.path_1))
+button_1.clicked.connect(lambda: button_clicked(path_1))
 
 button_2 = QPushButton("Play 2nd")
-button_2.clicked.connect(lambda: av_player.play_media(av_player.path_2))
+button_2.clicked.connect(lambda: button_clicked(path_2))
+
+
+def button_clicked(file_path):
+    # Remove previous player
+    # No effect adding "sleep(0.5)" between ".stop()", ".deleteLater()", ".pop(0)"
+    av_player_list[0].player.stop()
+    av_player_list[0].player.deleteLater()
+    av_player_list[0].video_output.deleteLater()
+    av_player_list.pop(0)
+    # Create new player and play media
+    av_player_list.append(AVPlayer())
+    vboxLayout.addWidget(av_player_list[0].video_output)
+    av_player_list[0].play_media(file_path)
+
 
 vboxLayout = QVBoxLayout(window)
-vboxLayout.addWidget(av_player.video_output)
 vboxLayout.addWidget(button_1)
 vboxLayout.addWidget(button_2)
+vboxLayout.addWidget(av_player_list[0].video_output)
 
 window.show()
 sys.exit(app.exec())
