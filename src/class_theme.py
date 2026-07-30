699b4b26bc59a74839f70e4833b386fb18b85505
k4karole <k4karole@gmail.com>
2026-07-30 07:58:08 +0100

    Theme class added

---
+++ src/class_theme.py
@@ -0,0 +1,172 @@
+from dataclasses import dataclass
+from pathlib import Path
+from json import load, dump
+import sys
+
+from PyQt6.QtGui import QPalette
+from PyQt6.QtWidgets import QApplication
+
+from .class_data import cv
+
+
+def open_skin_json():
+    with open(PATH_JSON_SETTINGS) as f:
+        json_dic = load(f)
+    return json_dic
+
+def save_skin_json():
+    with open(PATH_JSON_SETTINGS, 'w') as f:
+        dump(skin_settings, f, indent=2)
+    return
+
+if cv.skin_selected != 'system':
+    WORKING_DIRECTORY = Path().resolve()
+    PATH_JSON_SETTINGS = Path(WORKING_DIRECTORY, 'skins', 'jsons', f'{cv.skin_selected}.json')
+    skin_settings = open_skin_json()
+
+palette_dic = {
+    "text": QPalette.ColorRole.Text,
+    "base": QPalette.ColorRole.Base,
+    "alternate_base": QPalette.ColorRole.AlternateBase,
+    "button": QPalette.ColorRole.Button,
+    "button_text": QPalette.ColorRole.ButtonText,
+    "highlight": QPalette.ColorRole.Highlight,
+    "highlighted_text": QPalette.ColorRole.HighlightedText,
+    "mid": QPalette.ColorRole.Mid
+}
+
+
+@dataclass()
+class Theme:
+
+    if cv.skin_selected == 'system':
+        """
+        QApplication() has to be under the condition, otherwise the
+        "QApplication.setDesktopSettingsAware(False)" will not work
+        at the "src / application"
+        """
+        palette = QApplication(sys.argv).palette()
+
+        def get_color_hex(dic_key, palette=palette):
+            return (palette.color(palette_dic[dic_key])).name()
+
+        text = get_color_hex('text')
+        base = get_color_hex('base')
+        alternate_base = get_color_hex('alternate_base')
+        button = get_color_hex('button')
+        button_text = get_color_hex('button_text')
+        highlight = get_color_hex('highlight')
+        highlighted_text = get_color_hex('highlighted_text')
+        mid = get_color_hex('mid')
+
+        """ PLAYLIST ROWS """
+        row_inactive = base
+        row_inactive_text = text
+        row_selected = text
+        row_selected_text = base
+        row_playing = highlight
+        row_playing_text = highlighted_text
+
+        """ SLIDERS """
+        slider_duration_groove = alternate_base
+        slider_duration_sub_page = highlight
+        slider_duration_handle_pos_0_1 = alternate_base
+        slider_duration_handle_pos_2 = alternate_base
+
+        slider_volume_groove = alternate_base
+        slider_volume_sub_page = highlight
+        slider_volume_handle_pos_0_1 = alternate_base
+        slider_volume_handle_pos_2 = alternate_base
+
+        """ PLAYLIST TAB """
+        tab_playlist_bg_1 = mid
+        tab_playlist_bg_2 = mid
+        tab_playlist_bg_3 = alternate_base
+        tab_playlist_bg_4 = alternate_base
+        tab_playlist_border = mid
+        tab_playlist_font = text
+
+        tab_playlist_selected_bg = highlight
+        tab_playlist_selected_border = highlight
+        tab_playlist_selected_font = highlighted_text
+
+        duration_sum_widget_text = text
+
+
+        """ PLAYLIST BUTTONS - UNDER PLAYLISTS """
+        buttons_settings_bg_0 = mid
+        buttons_settings_bg_1 = mid
+        buttons_settings_bg_2 = alternate_base
+        buttons_settings_bg_3 = alternate_base
+        buttons_settings_border = text
+        buttons_settings_font = text
+        buttons_settings_pressed_bg = highlight
+
+
+        """ SETTINGS BUTTONS - UNDER PLAYLISTS """
+        buttons_playlist_bg_0 = mid
+        buttons_playlist_bg_1 = mid
+        buttons_playlist_bg_2 = alternate_base
+        buttons_playlist_bg_3 = alternate_base
+        buttons_playlist_border = text
+        buttons_playlist_font = text
+        buttons_playlist_pressed_bg = highlight
+
+
+
+    else:
+        """ PLAYLIST ROWS """
+        row_inactive = skin_settings['row_inactive']
+        row_inactive_text = skin_settings['row_inactive_text']
+        row_selected = skin_settings['row_selected']
+        row_selected_text = skin_settings['row_selected_text']
+        row_playing = skin_settings['row_playing']
+        row_playing_text = skin_settings['row_playing_text']
+
+        """ SLIDERS """
+        slider_duration_groove = skin_settings['slider_duration_groove']
+        slider_duration_sub_page = skin_settings['slider_duration_sub_page']
+        slider_duration_handle_pos_0_1 = skin_settings['slider_duration_handle_pos_0_1']
+        slider_duration_handle_pos_2 = skin_settings['slider_duration_handle_pos_2']
+
+        slider_volume_groove = skin_settings['slider_volume_groove']
+        slider_volume_sub_page = skin_settings['slider_volume_sub_page']
+        slider_volume_handle_pos_0_1 = skin_settings['slider_volume_handle_pos_0_1']
+        slider_volume_handle_pos_2 = skin_settings['slider_volume_handle_pos_2']
+
+        """ PLAYLIST TAB """
+        tab_playlist_bg_1 = skin_settings['tab_playlist_bg_1']
+        tab_playlist_bg_2 = skin_settings['tab_playlist_bg_2']
+        tab_playlist_bg_3 = skin_settings['tab_playlist_bg_3']
+        tab_playlist_bg_4 = skin_settings['tab_playlist_bg_4']
+        tab_playlist_border = skin_settings['tab_playlist_border']
+        tab_playlist_font = skin_settings['tab_playlist_font']
+
+        tab_playlist_selected_bg = skin_settings['tab_playlist_selected_bg']
+        tab_playlist_selected_border = skin_settings['tab_playlist_selected_border']
+        tab_playlist_selected_font = skin_settings['tab_playlist_selected_font']
+
+        duration_sum_widget_text = skin_settings['duration_sum_widget_text']
+
+
+        """ PLAYLIST BUTTONS - UNDER PLAYLISTS """
+        buttons_playlist_bg_0 = skin_settings['buttons_settings_bg_0']
+        buttons_playlist_bg_1 = skin_settings['buttons_settings_bg_1']
+        buttons_playlist_bg_2 = skin_settings['buttons_settings_bg_2']
+        buttons_playlist_bg_3 = skin_settings['buttons_settings_bg_3']
+        buttons_playlist_border = skin_settings['buttons_settings_border']
+        buttons_playlist_font = skin_settings['buttons_settings_font']
+        buttons_playlist_pressed_bg = skin_settings['buttons_settings_pressed_bg']
+
+
+        """ SETTINGS BUTTONS - UNDER PLAYLISTS """
+        buttons_settings_bg_0 = skin_settings['buttons_settings_bg_0']
+        buttons_settings_bg_1 = skin_settings['buttons_settings_bg_1']
+        buttons_settings_bg_2 = skin_settings['buttons_settings_bg_2']
+        buttons_settings_bg_3 = skin_settings['buttons_settings_bg_3']
+        buttons_settings_border = skin_settings['buttons_settings_border']
+        buttons_settings_font = skin_settings['buttons_settings_font']
+        buttons_settings_pressed_bg = skin_settings['buttons_settings_pressed_bg']
+
+
+tm = Theme()
